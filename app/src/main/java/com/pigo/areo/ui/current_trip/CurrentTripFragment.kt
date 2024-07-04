463b38b31088ed1034801e7aaa0163d7a9befd6d
PIGO <kariemseiam@gmail.com>
2024-07-04 22:28:53 +0300

    Integrate Data Binding in `CreateTripFragment`, enhance `CreateTripViewModel` with LiveData, and update layouts for binding and role-specific views.

--- app/src/main/java/com/pigo/areo/ui/current_trip/CurrentTripFragment.kt
+++ app/src/main/java/com/pigo/areo/ui/current_trip/CurrentTripFragment.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import com.pigo.areo.databinding.FragmentCurrentTripBinding
+import com.pigo.areo.shared.SharedViewModel
+import com.pigo.areo.shared.SharedViewModelFactory
 
 class CurrentTripFragment : Fragment() {
 
     private lateinit var binding: FragmentCurrentTripBinding
-    private val currentTripViewModel: CurrentTripViewModel by activityViewModels()
+    private val sharedViewModel: SharedViewModel by viewModels {
+        SharedViewModelFactory(binding.root.context.applicationContext)
+    }
+    private lateinit var gMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentCurrentTripBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
@@ -24,27 +32,35 @@ class CurrentTripFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Observe the GoogleMap instance from ViewModel
-        currentTripViewModel.googleMap.observe(viewLifecycleOwner) { googleMap ->
-            binding.fabCurrentLocation.setOnClickListener {
-                currentTripViewModel.moveCameraToCurrentPosition()
-            }
+        sharedViewModel.googleMap.observe(viewLifecycleOwner) { googleMap ->
+            gMap = googleMap
+/*
+            sharedViewModel.addMarkersToMap(googleMap)
+*/
+
+
         }
 
- /*       // Start location updates
-        viewModel.startLocationUpdates("locationKey")
+        binding.fabCurrentLocation.setOnClickListener {
+            sharedViewModel.loginAs(SharedViewModel.UserRole.PILOT)
+            if (::gMap.isInitialized) sharedViewModel.moveCameraToCurrentPosition()
+        }
 
-        // Add location change listener
-        viewModel.addLocationChangeListener("locationKey")
+        /*       // Start location updates
+               viewModel.startLocationUpdates("locationKey")
 
-        // Query location by key
-        viewModel.queryLocationByKey("locationKey") { latLng ->
-            if (latLng != null) {
-                // Do something with the LatLng
-                println("Location: $latLng")
-            } else {
-                println("Location not found")
-            }
-        }*/
+               // Add location change listener
+               viewModel.addLocationChangeListener("locationKey")
+
+               // Query location by key
+               viewModel.queryLocationByKey("locationKey") { latLng ->
+                   if (latLng != null) {
+                       // Do something with the LatLng
+                       println("Location: $latLng")
+                   } else {
+                       println("Location not found")
+                   }
+               }*/
 
         // Example of accessing FloatingActionButton and setting onClickListener
 

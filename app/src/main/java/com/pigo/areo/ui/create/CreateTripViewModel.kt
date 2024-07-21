0c222ded49b2e60bec40c5ba96262de243aeaa3c
Kariem Seiam <kariemseiam@gmail.com>
2024-07-21 21:04:32 +0300

    Update deployment target and improve UI functionality
    - Update camera movement delay for drivers to improve real-time responsiveness.
    - Disable unnecessary map UI elements, enhancing user experience.
    - Transition variable properties in `RouteCache` to immutable for better integrity.
    - Implement camera move listener in the `SharedViewModel` to dynamically track camera position.
    - Establish event listeners for incoming geo-queries and handle location updates more efficiently within the shared view model.
    - Refactor trip management functionalities with additional trip status flags and update the UI accordingly.
    - Streamline the trip details interface and enhance layout responsiveness across devices.

--- app/src/main/java/com/pigo/areo/ui/create/CreateTripViewModel.kt
+++ app/src/main/java/com/pigo/areo/ui/create/CreateTripViewModel.kt
@@ -4,13 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.firebase.geofire.GeoLocation
-import com.firebase.geofire.GeoQueryEventListener
 import com.google.android.gms.maps.model.LatLng
 import com.pigo.areo.geolink.GeolinkApiService
+import com.pigo.areo.geolink.models.ReverseGeocodeResponse
 import com.pigo.areo.geolink.models.SearchResult
 import com.pigo.areo.shared.SharedViewModel
-import com.pigo.areo.shared.SharedViewModel.UserRole
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -19,13 +17,10 @@ class CreateTripViewModel(private val sharedViewModel: SharedViewModel) : ViewMo
 
     private val geolinkApiService = GeolinkApiService()
 
-    private val _isDriver = MutableLiveData<Boolean>()
-    val isDriver: LiveData<Boolean> get() = _isDriver
-
     private val _pilotLocation = MutableLiveData<String>()
     val pilotLocation: LiveData<String> get() = _pilotLocation
 
-    private val _airportLocation = MutableLiveData<String>()
+    val _airportLocation = MutableLiveData<String>()
     val airportLocation: LiveData<String> get() = _airportLocation
 
     private val _isTripRunning = MutableLiveData<Boolean>()
@@ -37,112 +32,48 @@ class CreateTripViewModel(private val sharedViewModel: SharedViewModel) : ViewMo
     private val _selectAirportLocationEvent = MutableLiveData<Unit>()
     val selectAirportLocationEvent: LiveData<Unit> get() = _selectAirportLocationEvent
 
-    private val _pilotSearchResults = MutableLiveData<List<SearchResult>>()
-    val pilotSearchResults: LiveData<List<SearchResult>> get() = _pilotSearchResults
 
     private val _airportSearchResults = MutableLiveData<List<SearchResult>>()
     val airportSearchResults: LiveData<List<SearchResult>> get() = _airportSearchResults
 
-    private var pilotSearchJob: Job? = null
     private var airportSearchJob: Job? = null
 
     init {
-        observeUserRole()
         observeCurrentLocation()
     }
 
-    private fun observeUserRole() {
-        sharedViewModel.userRole.observeForever { userRole ->
-            val isDriver = userRole == UserRole.DRIVER
-            _isDriver.value = isDriver
 
-            val location =
-                if (isDriver) sharedViewModel.driverLatLng else sharedViewModel.pilotLatLng
-            location?.let {
-                reverseGeocodeLocation(it) { address ->
-                    setInitialLocation(
-                        address,
-                        isDriver
-                    )
-                }
-            }
-        }
-    }
-
-    private fun setInitialLocation(address: String, isDriver: Boolean) {
-        if (isDriver) {
-            _airportLocation.value = address
-        } else {
-            _pilotLocation.value = address
-        }
-    }
 
     private fun observeCurrentLocation() {
-        sharedViewModel.currentLatLng.observeForever { currentLatLng ->
-            _isDriver.value?.let { isDriver ->
-                val targetLatLng =
-                    if (isDriver) sharedViewModel.pilotLatLng else sharedViewModel.driverLatLng
-                targetLatLng?.let {
-                    if (isLocationArrived(currentLatLng, it)) {
-                        // Notify arrival at the location
-                    }
-                }
+
+        sharedViewModel.pilotLatLng?.let {
+            reverseGeocodeLocation(it) { address ->
+                _pilotLocation.value = address.data.address
             }
         }
+
     }
 
     fun onRoleChanged(isDriver: Boolean) {
-        _isDriver.value = isDriver
-        val userRole = if (isDriver) UserRole.DRIVER else UserRole.PILOT
-        sharedViewModel.loginAs(userRole)
-    }
-
-
-    fun updatePilotLocation(latLng: LatLng) {
-        sharedViewModel.updatePilotLatLng(latLng)
-    }
-
-    fun updateAirportLocation(latLng: LatLng) {
-        sharedViewModel.updateDriverLatLng(latLng)
+        sharedViewModel.loginAs(if (isDriver) SharedViewModel.UserRole.DRIVER else SharedViewModel.UserRole.PILOT)
     }
 
     fun toggleTripState() {
         val newState = !(_isTripRunning.value ?: false)
         _isTripRunning.value = newState
         if (newState) {
-            sharedViewModel.startLocationUpdates()
+            sharedViewModel.startTrip()
         } else {
+            sharedViewModel.stopTrip()
             sharedViewModel.removeLocation("pilot_location")
             sharedViewModel.removeLocation("airport_location")
+            sharedViewModel.removeLocation("driver_location")
         }
     }
 
-    fun selectPilotLocation() {
-        _selectPilotLocationEvent.value = Unit
-    }
 
     fun selectAirportLocation() {
         _selectAirportLocationEvent.value = Unit
-    }
-
-    fun confirmPilotLocation() {
-        sharedViewModel.pilotLatLng?.let {
-            sharedViewModel.updateLocation("pilot_location", GeoLocation(it.latitude, it.longitude))
-        }
-    }
-
-    fun confirmAirportLocation() {
-        sharedViewModel.driverLatLng?.let {
-            sharedViewModel.updateLocation(
-                "airport_location",
-                GeoLocation(it.latitude, it.longitude)
-            )
-        }
-    }
-
-    fun performPilotLocationSearch(query: String) {
-        pilotSearchJob?.cancel()
-        pilotSearchJob = performLocationSearch(query, _pilotSearchResults)
     }
 
     fun performAirportLocationSearch(query: String) {
@@ -150,58 +81,24 @@ class CreateTripViewModel(private val sharedViewModel: SharedViewModel) : ViewMo
         airportSearchJob = performLocationSearch(query, _airportSearchResults)
     }
 
-    private fun performLocationSearch(
-        query: String,
-        resultsLiveData: MutableLiveData<List<SearchResult>>
-    ): Job {
+    private fun performLocationSearch(query: String, resultsLiveData: MutableLiveData<List<SearchResult>>): Job {
         return viewModelScope.launch {
             delay(500)
             sharedViewModel.currentLatLng.value?.let { latLng ->
-                geolinkApiService.textSearch(query,
-                    latLng.latitude,
-                    latLng.longitude,
+                geolinkApiService.textSearch(query, latLng.latitude, latLng.longitude,
                     onSuccess = { response -> resultsLiveData.postValue(response.data) },
                     onFailure = { /* Handle error */ })
             }
         }
     }
 
-    private fun reverseGeocodeLocation(latLng: LatLng, callback: (String) -> Unit) {
+    fun reverseGeocodeLocation(latLng: LatLng, callback: (ReverseGeocodeResponse) -> Unit) {
         viewModelScope.launch {
             geolinkApiService.reverseGeocode(latLng.latitude, latLng.longitude).collect { result ->
                 result.onSuccess { response ->
-                    val address = response.data.address
-                    callback(address)
+                    callback(response)
                 }.onFailure { /* Handle failure */ }
             }
         }
-    }
-
-    private fun isLocationArrived(
-        currentLatLng: LatLng,
-        targetLatLng: LatLng,
-        threshold: Float = 50f
-    ): Boolean {
-        val results = FloatArray(1)
-        android.location.Location.distanceBetween(
-            currentLatLng.latitude,
-            currentLatLng.longitude,
-            targetLatLng.latitude,
-            targetLatLng.longitude,
-            results
-        )
-        return results[0] < threshold
-    }
-
-    fun createOrUpdateGeoQuery(id: String, center: GeoLocation, radius: Double) {
-        sharedViewModel.createOrUpdateGeoQuery(id, center, radius)
-    }
-
-    fun addGeoQueryEventListener(id: String, listener: GeoQueryEventListener) {
-        sharedViewModel.addGeoQueryEventListener(id, listener)
-    }
-
-    fun removeGeoQuery(id: String) {
-        sharedViewModel.removeGeoQuery(id)
     }
 }

fec8163914e57d3268b7805b067432cc77721ee0
Kariem Seiam <kariemseiam@gmail.com>
2024-07-27 10:00:43 +0300

    Refactor location updates and UI in MainActivity and CurrentTripFragment
    - Fetch last known location on permission grant in MainActivity
    - Refactor camera update logic to handle
     user roles
    - Update CurrentTripFragment to display route information from LiveData
    - Improve distance and duration formatting in CurrentTripFragment
    - Update FAB icon based on auto-move state in CurrentTripFragment

--- app/src/main/java/com/pigo/areo/ui/current_trip/CurrentTripFragment.kt
+++ app/src/main/java/com/pigo/areo/ui/current_trip/CurrentTripFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.GoogleMap
+import com.pigo.areo.R
 import com.pigo.areo.databinding.FragmentCurrentTripBinding
+import com.pigo.areo.geolink.models.Route
 import com.pigo.areo.shared.SharedViewModel
 import com.pigo.areo.shared.SharedViewModelFactory
 import com.pigo.areo.ui.create.CreateTripViewModel
@@ -57,16 +59,28 @@ class CurrentTripFragment : Fragment() {
         //        }
 
         sharedViewModel.routeResponse.observe(viewLifecycleOwner) { routeResponse ->
-            routeResponse?.let {
-                binding.tvIconLoc.text = routeResponse.distanceText
-                binding.tvIconTime.text = routeResponse.durationText
-            }
+            sharedViewModel.secRouteResponse.value?.let { secRouteResponse ->
+                updateUI(routeResponse, secRouteResponse)
+            } ?: updateUI(routeResponse, null)
+        }
+
+        sharedViewModel.secRouteResponse.observe(viewLifecycleOwner) { secRouteResponse ->
+            sharedViewModel.routeResponse.value?.let { routeResponse ->
+                updateUI(routeResponse, secRouteResponse)
+            } ?: updateUI(null, secRouteResponse)
         }
 
         binding.fabCurrentLocation.setOnClickListener {
-            sharedViewModel.currentLatLng.value?.let {
-                sharedViewModel.moveCameraToPosition(it)
+            val iconRes = if (!sharedViewModel.toggleAutoMoveEnabled()) {
+                R.drawable.ic_gps
+            } else {
+                R.drawable.ic_compass1
             }
+            sharedViewModel._selectingState.value = (iconRes == R.drawable.ic_gps)
+
+
+            binding.fabCurrentLocation.setImageResource(iconRes)
+            sharedViewModel.updateCameraPosition()
         }
 
 
@@ -84,5 +98,48 @@ class CurrentTripFragment : Fragment() {
         sharedViewModel.removeGeoQuery("airport_location")
         sharedViewModel.removeGeoQuery("driver_location")
     }
+
+    private fun formatDuration(seconds: Int): String {
+        val minutes = seconds / 60
+        val hours = minutes / 60
+        return when {
+            hours > 0 -> "$hours h ${minutes % 60} min"
+            minutes > 0 -> "$minutes min"
+            else -> "$seconds sec"
+        }
+    }
+
+    private fun formatDistance(meters: Int): String {
+        val kilometers = meters / 1000.0
+        return when {
+            kilometers <= 0.5 -> {
+                // Show as "0.5 km" for values <= 500 meters
+                "0.5 km"
+            }
+
+            kilometers <= 1.0 -> {
+                // Show as "1.0 km" for values > 500 meters and <= 1000 meters
+                "1.0 km"
+            }
+
+            else -> {
+                // For distances greater than 1000 meters, show rounded km value
+                val roundedKilometers = Math.round(kilometers).toInt()
+                "$roundedKilometers km"
+            }
+        }
+    }
+
+
+    private fun updateUI(routeResponse: Route?, secRouteResponse: Route?) {
+        val totalDistanceMeters =
+            (routeResponse?.distanceMeters ?: 0) + (secRouteResponse?.distanceMeters ?: 0)
+        val totalDurationSeconds =
+            (routeResponse?.durationSeconds ?: 0) + (secRouteResponse?.durationSeconds ?: 0)
+
+        binding.tvIconLoc.text = formatDistance(totalDistanceMeters)
+        binding.tvIconTime.text = formatDuration(totalDurationSeconds)
+    }
+
 }
 

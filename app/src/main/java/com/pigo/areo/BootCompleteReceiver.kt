897da3cd490f768daadacc3afef69d664c80ff73
kariemSeiam <kariemSeiam@users.noreply.github.com>
2024-07-28 16:18:56 +0300

    feat: Migrate SharedPreferences to DataStore for trip and user role persistence
    This commit migrates the storage of trip and user role data from SharedPreferences to Data
    Store for improved efficiency and type safety.
    
    - Introduced DataStoreUtil for handling data persistence with DataStore.
    - Updated LocationService to use DataStore for retrieving and saving trip and user role information.
    - Added PreferencesKeys object to define keys for DataStore entries.
    - Created BootCompleteReceiver to restart Location
    Service on device boot.

---
+++ app/src/main/java/com/pigo/areo/BootCompleteReceiver.kt
@@ -0,0 +1,22 @@
+package com.pigo.areo
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+class BootCompleteReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+
+            // Start your LocationService
+            val serviceIntent = Intent(context, LocationService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ContextCompat.startForegroundService(context!!, serviceIntent)
+            } else {
+                context?.startService(serviceIntent)
+            }
+        }
+    }
+}
